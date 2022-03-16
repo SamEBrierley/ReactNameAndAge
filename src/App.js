b96ede731076ca108072258bfac0d6cd60f38b5b
SamEBrierley <samellisbrierley@gmail.com>
2022-03-16 10:03:56 +0000

    Added a basic user input form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import AddUser from './components/Users/AddUser';
 
 function App() {
   return (
     <div>
       <h2>Bonjour World</h2>
+      <AddUser/>
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,7 +2,18 @@ import React from 'react';
 import style from './AddUser.module.css'
 
 const AddUser = () => {
-
+    const addUserHandler = (event)=>{
+        event.preventDefault();
+    }
+    return(
+        <form onSubmit={addUserHandler}>
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" />
+            <label htmlFor="age">Age (Years)</label>
+            <input id="age" type="number" />
+            <button type="submit">Add User</button> 
+        </form>
+    )
 }
 
 export default AddUser;
